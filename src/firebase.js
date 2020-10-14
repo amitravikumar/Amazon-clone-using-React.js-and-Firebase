f6a8fecac41fba65b86c8487570228188c68049a
Amit Kumar <besttechamit@gmail.com>
2020-10-14 22:46:34 +0530

    Basic First step files added.

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCtREm3MkHVcXj1q1dq8cHUxQM5tTziPFQ",
+    authDomain: "clone-e486a.firebaseapp.com",
+    databaseURL: "https://clone-e486a.firebaseio.com",
+    projectId: "clone-e486a",
+    storageBucket: "clone-e486a.appspot.com",
+    messagingSenderId: "975541047494",
+    appId: "1:975541047494:web:cb2e636a2873adf83b47c7",
+    measurementId: "G-NNT7VJ5KJS"
+};
