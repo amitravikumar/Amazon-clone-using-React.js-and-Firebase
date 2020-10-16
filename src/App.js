071efc153eeb57edfdc4451f7c9a171ce88a0578
Amit Kumar <besttechamit@gmail.com>
2020-10-16 10:10:08 +0530

    Home component added and other components modified.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-
+import Home from './Home';
 function App() {
   return (
       //BEM Convention for CSS
     <div className="app">
         <Header />
         {/* Header*/}
+        <Home />
         {/* Home */}
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
     return(
@@ -8,7 +9,7 @@ function Header() {
             <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
 
             <div className="header__search">
-                <input className="header__searchinput" />
+                <input className="header__searchInput" />
                 <SearchIcon className="header__searchIcon" />
                 {/* Logo */}
             </div>
@@ -25,6 +26,10 @@ function Header() {
                 <div className="header__option">
                     <span className="header__optionLineOne">Your</span>
                     <span className="header__optionLineTwo">Prime</span>
+                </div>
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLineTwo header__basketCount">0</span>
                 </div>
             </div>
         </div>
