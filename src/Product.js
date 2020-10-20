71c5f88ad47820fc7f924f3c9703bad1c79c520a
Amit Kumar <besttechamit@gmail.com>
2020-10-20 09:33:15 +0530

    More files added

--- src/Product.js
+++ src/Product.js
@@ -10,7 +10,14 @@ function Product (){
                     <small>Rs.</small>
                     <strong>325</strong>
                 </p>
+                <div className="product__rating">
+                    <p>⭐</p>
+                    <p>⭐</p>
+                    <p>⭐</p>
+                </div>
             </div>
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg" alt="An image was supposed to be here"/>
+            <button>Add to Basket</button>
         </div>
     )
 }
