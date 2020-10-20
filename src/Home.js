298163229c94d55502a2e82d4631f329a494f61e
Amit Kumar <besttechamit@gmail.com>
2020-10-20 11:45:10 +0530

    more components added and updated

--- src/Home.js
+++ src/Home.js
@@ -9,18 +9,27 @@ function Home() {
                 <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="Amazon Banner Image was supposed to be here"/>
 
                 <div className="home__row">
+                    <Product
+                    title= "The Lean StartUp"
+                    price={325}
+                    image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+                    rating={5} />
                     <Product />
                     {/* Product 1*/}
                     {/* Product 2*/}
                 </div>
 
                 <div className="home__row">
+                    <Product />
+                    <Product />
+                    <Product />
                     {/* Product 1*/}
                     {/* Product 2*/}
                     {/* Product 3*/}
                 </div>
                 
                 <div className="home__row">
+                    <Product />
                     {/* Product 1*/}
                 </div>
             </div>
--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './Product.css';
 
-function Product (){
+function Product ({title, image, price, rating}){
     return(
         <div className="product">
             <div className="product__info">
-                <p>The Startup guide</p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>Rs.</small>
-                    <strong>325</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
-                    <p>⭐</p>
-                    <p>⭐</p>
-                    <p>⭐</p>
+                    {Array(rating).fill().map((_,i) => (
+                        <p>⭐</p>
+                    ))}
                 </div>
             </div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg" alt="An image was supposed to be here"/>
+            <img>{image}</img>
             <button>Add to Basket</button>
         </div>
     )
