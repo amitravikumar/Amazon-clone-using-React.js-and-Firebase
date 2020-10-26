d2c6e4aa545a188a63bb3e17e9d6d47799bea4ab
Amit Kumar <besttechamit@gmail.com>
2020-10-26 10:43:48 +0530

    Product page updated and more products added.

--- src/Product.js
+++ src/Product.js
@@ -16,7 +16,7 @@ function Product ({title, image, price, rating}){
                     ))}
                 </div>
             </div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg" alt="An image was supposed to be here"/>
+            <img src={image} alt="An image was supposed to be here"/>
             <button>Add to Basket</button>
         </div>
     )
