355f1f27d3ae98b657facda68d6a499f1599791a
Amit Kumar <besttechamit@gmail.com>
2020-10-26 11:10:13 +0530

    Router elements adding and react-router-dom introduced.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
 function App() {
   return (
-      //BEM Convention for CSS
-    <div className="app">
-        <Header />
-        {/* Header*/}
-        <Home />
-        {/* Home */}
-    </div>
+      <Router>
+        {/*BEM Convention for CSS*/}
+        <div className="app">
+           <Switch>
+            <Route path="/Checkout">
+               <Header />
+                <h1> hey I am, the checkout page. Lets code this complete.</h1>
+            </Route>
+            <Route path="/">
+              <Header />
+               {/* Header*/}
+               <Home />
+               {/* Home */}
+            </Route>
+           </Switch>
+        </div>
+      </Router>  
   );
 }
 
