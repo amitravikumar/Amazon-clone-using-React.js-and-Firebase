bb37f0811cbc94e9f155bc48b644e3b27f7f8119
Amit Kumar <besttechamit@gmail.com>
2020-10-27 22:08:41 +0530

    Product page updated.

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -4,6 +4,18 @@ import './CheckoutProduct.css';
 function CheckoutProduct = ({ id, image, title, price, rating}) => {
     return( <div className="checkoutProduct">
             <img className="checkoutProduct__image" src={image} />
+            <div className="checkoutProduct__info">
+                <p className="checkoutProduct__title">{title}</p>
+                <p className="checkoutProduct__price">
+                    <small>₹</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="checkoutProduct__rating">
+                    {Array(rating).fill().map((-,i) => {
+                        <p>🌟</p>
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
