6aeec456be46b676924540b9e4d8a1f6e30e6d28
Amit Kumar <besttechamit@gmail.com>
2020-10-31 15:12:05 +0530

    MInor updates

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -20,14 +20,14 @@ function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
             <div className='checkoutProduct__info'>
                 <p className='checkoutProduct__title'>{title}</p>
                 <p className="checkoutProduct__price">
-                    <small>$</small>
+                    <small>₹</small>
                     <strong>{price}</strong>
                 </p>
                 <div className="checkoutProduct__rating">
                     {Array(rating)
                         .fill()
                         .map((_, i) => (
-                            <p>🌟</p>
+                            <p>⭐</p>
                         ))}
                 </div>
                 {!hideButton && (
